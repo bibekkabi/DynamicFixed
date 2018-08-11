f31efeca2b0dac29d3272e8df85f5e9d69462b9b
Bibek Kabi <bibek@lix.polytechnique.fr>
2018-08-12 00:58:30 +0200

    Add files via upload

---
+++ Fixed_dyn.h
@@ -0,0 +1,31 @@
+typedef unsigned long int FP32 ; // unsignd long int 32 bit in size
+
+#define	INF_FP32	0x7fffffffUL
+#define	NINF_FP32	0xffffffffUL
+
+#define	FP32_ZERO	0x00000000UL
+#define FP32_MSB	0x80000000UL
+
+#define	FP32_LO_WORD(x)		(*((unsigned short int *) (&x)))		// the type of FP32_32 is ULL, so it divides integer part to HI word,
+#define	FP32_HI_WORD(x)		(*((unsigned short int *) (&x) + 1))	// and exponetial part to LO word. that why he used UL to represent value.
+#define	FP32_SIGN(x)			(x & 0x80000000UL)	
+#define	FP32_CLEAR_SIGN(x)	(x &= 0x7fffffffUL)	// 
+#define	FP32_SET_SIGN(x)		(x|=0x80000000UL)  // 
+#define	FP32_PUT_SIGN(x,y)	if(y){FP32_SET_SIGN(x);}else{FP32_CLEAR_SIGN(x);}
+#define	FP32_TOGGLE_SIGN(x)	(x^=0x80000000)
+#define	FP32_INTEGER(x)		(x & 0x7fffffffUL)>>Q
+#define	ONE		0x00000001UL
+
+FP32 fconv( FP32 a , FP32 q1 , FP32 q2);// Convrsion from q1 format to q2 format
+FP32 Get_Optimum_Q(float flt_a);// Get Q format depending on range
+FP32 tofix(float a, FP32 q); // Floating-point to fixd-point format(q) 
+float toflt(FP32 a , FP32 q); // Fixed-point (q) to floating-point format
+
+FP32 FP32_add (FP32 num1, FP32 num2, FP32 Q1, FP32 Q2, FP32 *Q) ;
+FP32 FP32_subtract (FP32 num1, FP32 num2, FP32 Q1, FP32 Q2, FP32 *Q) ;
+FP32 FP32_mult (FP32 num1, FP32 num2, FP32 Q1, FP32 Q2, FP32 *Q) ;
+FP32 FP32_div (FP32 num1, FP32 num2, FP32 Q1, FP32 Q2, FP32 *Q) ;
+//FP32 sqrtx (FP32 a, FP32 Q) ;
+//FP32 Fsqrt(FP32 M , FP32 Q);
+FP32 adjust(FP32 x, FP32 Q);
+FP32 APP_sqrt(FP32 x , FP32* Q);
